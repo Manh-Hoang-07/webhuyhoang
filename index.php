2fd5e4b6b999fd89e4bd28eb41df4e43465c314a
Manh-Hoang-07 <43363434+Manh-Hoang-07@users.noreply.github.com>
2019-12-17 22:24:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once "account/control/controlAccount.php";
+class aba {
+	public function bab() {
+		$adw=new AccountControl();
+		$adw->checkdangnhap();
+	}
+}
+$ddc=new aba();
+$ddc->bab();
+?>
